b76b099382617556af85e738ec10c0024b454484
Manpreet Singh <mskhurana729@gmail.com>
2025-02-20 00:21:14 -0500

    feat: Add input for city

--- webpack.common.js
+++ webpack.common.js
@@ -5,14 +5,27 @@ module.exports = {
   entry: {
     app: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Production",
-    }),
-  ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
