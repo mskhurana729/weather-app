461586a6e67ea4622dbd023ba6b2942cee5bc34a
Manpreet Singh <mskhurana729@gmail.com>
2025-02-22 16:50:48 -0500

    feat: Render weather for valid searched city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 (() => {
   const searchBox = document.querySelector("#searchBox");
   const searchBtn = document.querySelector(".searchBtn");
+  const renderWeatherArea = document.querySelector(".renderWeatherArea");
 
   async function fetchCityWeather(cityName) {
     try {
@@ -19,6 +20,7 @@
   async function dataToJson(response) {
     try {
       const data = await response.json();
+      console.log(data.address);
       return data;
     } catch (e) {
       console.log(e);
@@ -27,15 +29,68 @@
 
   function displayData(data) {
     if (data) {
-      console.log(data, "Temp: " + data.currentConditions.feelslike);
+      console.log("Temp: " + data);
     }
   }
   searchBtn.addEventListener("click", async () => {
     const cityName = searchBox.value;
+    let time = performance.now();
     const response = await fetchCityWeather(cityName);
     const data = await dataToJson(response);
-
+    let requestTime = performance.now() - time;
+    console.log(requestTime);
+    renderWeather(data);
     displayData(data);
     searchBox.value = "";
   });
+
+  function renderWeather(data) {
+    try {
+      renderWeatherArea.textContent = "";
+      let addressNode = document.createElement("p");
+      addressNode.textContent = data.address.toUpperCase();
+
+      let tempNode = document.createElement("p");
+
+      tempNode.textContent = "Temp: " + data.currentConditions.temp + "`F";
+      console.log(tempNode);
+      tempNode.classList.add("tempNode");
+      let changeTempTypeBtn = document.createElement("button");
+      changeTempTypeBtn.classList.add("FToC");
+      changeTempTypeBtn.classList.add("changeTypeBtn");
+      changeTempTypeBtn.textContent = "F -> C";
+      changeTempTypeBtnEvent(
+        tempNode,
+        changeTempTypeBtn,
+        data.currentConditions.temp,
+      );
+      renderWeatherArea.appendChild(addressNode);
+      renderWeatherArea.appendChild(tempNode);
+      renderWeatherArea.appendChild(changeTempTypeBtn);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function changeTempTypeBtnEvent(tempNode, btn, tempInF) {
+    btn.addEventListener("click", (e) => {
+      if (btn.classList.contains("FToC")) {
+        btn.classList.remove("FToC");
+        btn.classList.add("CToF");
+        console.log(btn.classList);
+        btn.textContent = "C -> F";
+        let tempInC = (((+tempInF - 32) * 5) / 9).toFixed(2);
+        tempNode.textContent = "Temp: " + tempInC + "`C";
+
+        console.log(tempInF);
+      } else {
+        btn.classList.add("FToC");
+        btn.classList.remove("CToF");
+        btn.textContent = "F -> C";
+        tempNode.textContent = "Temp: " + tempInF + "`F";
+
+        console.log(tempInF);
+      }
+    });
+  }
 })();
